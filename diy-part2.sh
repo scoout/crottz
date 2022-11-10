4bb632e11b9a12988ddef1e1cd22489f935e8d53
iqbal fasya <79214502+scoout@users.noreply.github.com>
2022-11-11 03:32:59 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,15 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+set -ex
+echo '
+CONFIG_MMC_TEST=y
+CONFIG_MMC_DEBUG=y
+CONFIG_MMC_SDHCI_ACPI=y
+CONFIG_MMC_TIFM_SD=y
+CONFIG_TIFM_CORE=y
+CONFIG_TIFM_7XX1=y
+' >> ./target/linux/x86/generic/config-5.15
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate

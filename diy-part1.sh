63f55ba222c3316d4e4742363728481dcf8226b7
iqbal fasya <79214502+scoout@users.noreply.github.com>
2022-11-11 03:30:48 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,12 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo '
-CONFIG_MMC_TEST=y
-CONFIG_MMC_DEBUG=y
-CONFIG_MMC_SDHCI_ACPI=y
-CONFIG_MMC_TIFM_SD=y
-CONFIG_TIFM_CORE=y
-CONFIG_TIFM_7XX1=y
-' >> ./target/linux/x86/generic/config-5.15
+
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default

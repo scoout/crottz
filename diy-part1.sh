3d99d7dee2db972ae896e315ca6e1b74289691ff
iqbal fasya <79214502+scoout@users.noreply.github.com>
2023-04-04 13:17:46 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
